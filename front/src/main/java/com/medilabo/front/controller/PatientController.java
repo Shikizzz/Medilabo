cadb2f45312ea7ae0e9e51b01b97827ed947f444
Shikizzz <widemann.simon@outlook.fr>
2024-06-11 11:09:09 +0200

    Sprint 1 finalized

--- front/src/main/java/com/medilabo/front/controller/PatientController.java
+++ front/src/main/java/com/medilabo/front/controller/PatientController.java
@@ -4,13 +4,20 @@ import com.medilabo.front.model.Patient;
 import com.medilabo.front.model.PatientDTO;
 import com.medilabo.front.service.PatientService;
 import jakarta.validation.Valid;
+import org.springframework.expression.spel.SpelEvaluationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.exceptions.TemplateProcessingException;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Controller
@@ -21,10 +28,16 @@ public class PatientController {
     }
 
     @GetMapping("/patients")
-    public String getPatients(@RequestParam Optional<String> search, Model model) {
+    public String getPatients(@RequestParam Optional<String> search, @RequestParam Optional<String> error, Model model) {
+        if (error.isPresent()) {
+            model.addAttribute("illegalArgumentError", true);
+        }
         if(search.isPresent()) {
             List<Patient> patients = patientService.getPatientsListBySearchedString(search.get());
-            model.addAttribute("patients", patients);
+            if(patients.size()>0){
+                model.addAttribute("patients", patients);
+            }
+            else model.addAttribute("noPatientFoundError", true);
         }
         return "patients.html";
     }
@@ -37,27 +50,38 @@ public class PatientController {
     }
 
     @GetMapping("/addPatient")
-    public String getAddPatient() {
+    public String getAddPatient(@RequestParam Optional<String> error, Model model) {
+        if(error.isPresent()){
+            model.addAttribute("invalidPatientError", true);
+        }
         return "addPatient.html";
     }
     @PostMapping("/addPatient")
-    public String postAddPatient(@ModelAttribute(name = "patientInfo") PatientDTO patient) throws IOException {
+    public String postAddPatient(@Valid @ModelAttribute(name = "patientInfo") PatientDTO patient, Model model) throws IOException {
         patientService.postPatient(patient);
         return "redirect:patients";
     }
 
     @GetMapping("/patientInfo")
     public String getPatient(@RequestParam Integer id, Model model) {
-        Patient patient = patientService.getPatientById(id);
-        model.addAttribute(patient);
+        try{
+            Patient patient = patientService.getPatientById(id);
+            model.addAttribute(patient);
+        }
+        catch (IllegalArgumentException e){
+            return "redirect:patients?error=illegalArgument";
+        }
         return "patientInfo.html";
     }
 
     @GetMapping("/editPatient")
     public String getEditPatient(@RequestParam Integer id, Model model){
-        Patient patient = patientService.getPatientById(id);
-        model.addAttribute("patient", patient);
-        return "editPatient.html";
+            Patient patient = patientService.getPatientById(id);
+            if(patient != null){
+                model.addAttribute("patient", patient);
+                return "editPatient.html";
+            }
+            return "redirect:patients?error=illegalArgument";
     }
     @PostMapping("/editPatient")
     public String putEditPatient(@Valid @ModelAttribute(name = "patientInfo") PatientDTO patientDTO) throws IOException {
@@ -65,10 +89,15 @@ public class PatientController {
         return "redirect:patients";
     }
 
-    @GetMapping("/deletePatient")  //Is it a good way to do it ?
+    @GetMapping("/deletePatient")
     public String deletePatient(@RequestParam Integer id){
         patientService.deletePatient(id);
         return "redirect:patients";
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String handleValidationErrors() {
+        return "redirect:addPatient?error=invalid";
     }
 
 }
